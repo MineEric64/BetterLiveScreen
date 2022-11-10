0de27c2eea6be1c445dd26fc70291cda8ded6c4f
MineEric64 <besteric40@gmail.com>
2022-11-11 06:27:19 +0900

    singihada

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -380,8 +380,7 @@ namespace BetterLiveScreen
                                 int length = Rescreen.ScreenActualSize.Width * Rescreen.ScreenActualSize.Height * 4;
 
                                 Marshal.Copy(preview, 0, mat.Data, length);
-                                Mat mat2 = mat.CvtColor(ColorConversionCodes.BGRA2BGR);
-                                Mat mat3 = mat2.CvtColor(ColorConversionCodes.BGRA2YUV_I420);
+                                Mat mat2 = mat.CvtColor(ColorConversionCodes.BGRA2YUV_I420);
 
                                 unsafe
                                 {
